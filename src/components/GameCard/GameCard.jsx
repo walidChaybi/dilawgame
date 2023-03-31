a9859f4d80cbb1c1b42c7803b370f39ee0dd4963
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2023-03-31 15:55:31 +0200

    implement cropping image func

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -2,11 +2,11 @@ import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import PlatformIconList from "../PlatformIconList/PlatformIconList";
 import CriticScore from "../CriticScore/CriticScore";
-
+import getCroppedImage from "../../utils/getCroppedImage";
 function GameCard({ game }) {
   return (
     <Card borderRadius="15px" overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImage(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import ColorModeSwitch from "../ColorModeSwitch/ColorModeSwitch";
 import logo from "../../assets/logo.png";
 function Navbar() {
   return (
-    <HStack bg="rgb(20,15,55)" padding="10px" justifyContent="space-between">
+    <HStack padding="10px" justifyContent="space-between">
       <Image boxSize="150px" objectFit="contain" src={logo} />
       <ColorModeSwitch />
     </HStack>

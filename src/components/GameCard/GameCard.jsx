5c14b0fbc6b4ec60423c90cbfdcf683f12b2d204
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2023-03-31 15:30:27 +0200

    displaying platform icons

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import PlatformIconList from "../PlatformIconList/PlatformIconList";
 
 function GameCard({ game }) {
   return (
@@ -7,6 +8,7 @@ function GameCard({ game }) {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList game={game} />
       </CardBody>
     </Card>
   );
